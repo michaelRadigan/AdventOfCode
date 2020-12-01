5a00401abd4bcf9ff26c332872ac9c981cc75eb0
Michael Radigan <michael@radigan.co.uk>
2020-12-01 21:09:54 +0000

    Reuse part a in solution to part b

--- 2020/1/solve.py
+++ 2020/1/solve.py
@@ -2,29 +2,27 @@ import os
 import collections
 
 
-def solve_a(entries):
+def solve_a(entries, target):
     seen = set()
     for entry in entries:
-        partner = 2020 - entry
+        partner = target - entry
         if partner in seen:
-            return partner*entry
+            return True, partner*entry
         seen.add(entry)
-    return -1
+    return False, -1
 
 
 def solve_b(entries):
     for i, entry in enumerate(entries):
-        seen = set()
         target = 2020 - entry
-        for partner in entries[i+1:]:
-            if (target - partner) in seen:
-                return entry*partner*(target-partner)
-            seen.add(partner)
-    return -1
+        possible, product = solve_a(entries[i+1:], target)
+        if possible:
+            return True, product*entry
+    return False, -1
 
 
 if __name__ == "__main__":
     with open("2020/1/input.txt", "r") as f:
         entries = [int(x) for x in f.read().split()]
-    print(f"Part A: {solve_a(entries)}")
+    print(f"Part A: {solve_a(entries, 2020)}")
     print(f"Part B: {solve_b(entries)}")
