1b06f833802fa99a4e54f88b1f4f0c98f1f6be6d
Michael Radigan <michael@radigan.co.uk>
2020-12-01 20:40:26 +0000

    Initial solution to 2020/1/b

--- 2020/1/solve.py
+++ 2020/1/solve.py
@@ -12,7 +12,19 @@ def solve_a(entries):
     return -1
 
 
+def solve_b(entries):
+    for i, entry in enumerate(entries):
+        seen = set()
+        target = 2020 - entry
+        for partner in entries[i+1:]:
+            if (target - partner) in seen:
+                return entry*partner*(target-partner)
+            seen.add(partner)
+    return -1
+
+
 if __name__ == "__main__":
     with open("2020/1/input.txt", "r") as f:
         entries = [int(x) for x in f.read().split()]
     print(f"Part A: {solve_a(entries)}")
+    print(f"Part B: {solve_b(entries)}")
