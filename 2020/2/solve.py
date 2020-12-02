5a4da04c2c51e4a58be09427f3f04ff49e1d414f
Michael Radigan <michael@radigan.co.uk>
2020-12-02 18:40:30 +0000

    Remove duplication between solva_a and solve_b + general clean up

--- 2020/2/solve.py
+++ 2020/2/solve.py
@@ -2,23 +2,22 @@ import collections
 from aocd.models import Puzzle
 
 
-def parse_line(line):
-    nums, target, password = line.split(' ')
-    i, j = nums.split('-')
-    return int(i), int(j), target[0], password
-
-
 def parse(input):
+    def parse_line(line):
+        nums, target, password = line.split(' ')
+        i, j = nums.split('-')
+        return int(i), int(j), target[0], password
+
     return map(parse_line, input.split('\n'))
+
+
+def solve(password_infos, is_valid):
+    return sum(map(is_valid, password_infos))
 
 
 def is_valid_a(password_info):
     min_target, max_target, target, password = password_info
     return min_target <= password.count(target) <= max_target
-
-
-def solve_a(password_infos):
-    return sum(map(is_valid_a, password_infos))
 
 
 def is_valid_b(password_info):
@@ -29,17 +28,13 @@ def is_valid_b(password_info):
     return is_match(i, target, password) != is_match(j, target, password)
 
 
-def solve_b(password_infos):
-    return sum(map(is_valid_b, password_infos))
-
-
 if __name__ == "__main__":
     puzzle = Puzzle(year=2020, day=2)
     password_infos = list(parse(puzzle.input_data))
-    solution_a = solve_a(password_infos)
+    solution_a = solve(password_infos, is_valid_a)
     print(f"Part A: {solution_a}")
     puzzle.answer_a = solution_a
 
-    solution_b = solve_b(password_infos)
+    solution_b = solve(password_infos, is_valid_b)
     print(f"Part B: {solution_b}")
     puzzle.answer_b = solution_b
