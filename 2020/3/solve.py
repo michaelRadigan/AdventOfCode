c94c1a7968cac5e08a451f15a6d687ffa1d58886
Michael Radigan <michael@radigan.co.uk>
2020-12-03 08:47:51 +0000

    Initial somewhat messy solution to part b

--- 2020/3/solve.py
+++ 2020/3/solve.py
@@ -1,14 +1,24 @@
 import collections
 from aocd.models import Puzzle
+import itertools
 
 
-def is_hash(iline):
-    i, line = iline
-    return line[i*3 % len(line)] == '#'
+def is_hash(i, line, dx, dy):
+    return line[i*dx % len(line)] == '#'
 
 
 def solve_a(input_data):
-    return sum(map(is_hash, enumerate(input_data.split('\n'))))
+    return sum(map(lambda iline: is_hash(iline[0], iline[1], 3, 1), enumerate(input_data.split('\n'))))
+
+
+def solve_b(input_data):
+    slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    product = 1
+    for dx, dy in slopes:
+        num_trees_for_slope = sum(map(lambda iline: is_hash(
+            iline[0], iline[1], dx, dy), enumerate(input_data.split('\n')[::dy])))
+        product *= num_trees_for_slope
+    return product
 
 
 if __name__ == "__main__":
@@ -17,3 +27,7 @@ if __name__ == "__main__":
     num_trees_a = solve_a(puzzle.input_data)
     print(f"Part A: {num_trees_a}")
     puzzle.answer_a = num_trees_a
+
+    product_trees_b = solve_b(puzzle.input_data)
+    print(f"Part B: {product_trees_b}")
+    puzzle.answer_b = product_trees_b
