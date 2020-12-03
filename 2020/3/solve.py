39c81a841e1f1c731c905fe5bdec67ceb0564957
Michael Radigan <michael@radigan.co.uk>
2020-12-03 09:07:04 +0000

    Remove duplication between part A and part B

--- 2020/3/solve.py
+++ 2020/3/solve.py
@@ -7,27 +7,23 @@ def is_hash(i, line, dx, dy):
     return line[i*dx % len(line)] == '#'
 
 
-def solve_a(input_data):
-    return sum(map(lambda iline: is_hash(iline[0], iline[1], 3, 1), enumerate(input_data.split('\n'))))
-
-
-def solve_b(input_data):
-    slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+def solve(input_data, slopes):
     product = 1
     for dx, dy in slopes:
-        num_trees_for_slope = sum(map(lambda iline: is_hash(
-            iline[0], iline[1], dx, dy), enumerate(input_data.split('\n')[::dy])))
-        product *= num_trees_for_slope
+        ilines = enumerate(input_data.split('\n')[::dy])
+        product *= sum(map(
+            lambda iline: is_hash(iline[0], iline[1], dx, dy), ilines))
     return product
 
 
 if __name__ == "__main__":
     puzzle = Puzzle(year=2020, day=3)
 
-    num_trees_a = solve_a(puzzle.input_data)
+    num_trees_a = solve(puzzle.input_data, [(3, 1)])
     print(f"Part A: {num_trees_a}")
     puzzle.answer_a = num_trees_a
 
-    product_trees_b = solve_b(puzzle.input_data)
+    slopes_b = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    product_trees_b = solve(puzzle.input_data, slopes_b)
     print(f"Part B: {product_trees_b}")
     puzzle.answer_b = product_trees_b
