8aa8cc71d25c8021a41519d1caf6a4ae735f88a4
Michael Radigan <michael@radigan.co.uk>
2020-12-06 23:52:29 +0000

    Add lazy solution to part 6 b

--- 2020/6/solve.py
+++ 2020/6/solve.py
@@ -8,7 +8,11 @@ if __name__ == "__main__":
     total_count_a = sum(map(
         lambda group: len(set(group.replace('\n', ''))), puzzle.input_data.split('\n\n')))
 
-    print(f"{total_count=}")
-    puzzle.answer_a = total_count
-    # print(f"{my_allocation=}")
-    # puzzle.answer_b=my_allocation
+    # Part b also seems a bit lame, filthy one liner here as well with whatever formatting PEP8 decides, I'm sorry
+    total_count_b = sum(map(lambda answers: len(functools.reduce(set.intersection, answers)), map(
+        lambda group: map(set, group.split('\n')), puzzle.input_data.split('\n\n'))))
+
+    print(f"{total_count_a=}")
+    puzzle.answer_a = total_count_a
+    print(f"{total_count_b=}")
+    puzzle.answer_b = total_count_b
