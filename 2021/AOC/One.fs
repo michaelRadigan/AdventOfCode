4a97c1e6a688ba847c8b1426c05f9c25c5fbfab1
Michael Radigan <michael@radigan.co.uk>
2021-12-02 23:39:06 +0000

    Start F# and do day one

--- 2021/AOC/One.fs
+++ 2021/AOC/One.fs
@@ -1,2 +1,15 @@
 module AOC.one
+    let solveA (lines : string seq) =
+        lines
+        |> Seq.map int
+        |> Seq.pairwise
+        |> Seq.filter (fun (current, next) -> next > current)
+        |> Seq.length
+                   
+    let solveB (lines : string seq) =
+        let intLines = lines |> Seq.map int
+        Seq.zip intLines (intLines |> Seq.skip 3)
+        |> Seq.filter (fun (current, next) -> next > current)
+        |> Seq.length
+        
 
---
+++ 2021/AOC/Program.fs
@@ -0,0 +1,25 @@
+// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+open System.IO
+open System.Reflection
+
+[<EntryPoint>]
+let main _ =
+    let day = 1    
+
+    // This is to get up from the exe directory    
+    let projectName = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName
+    let path = Path.Combine(projectName, "Inputs", $"{day}.in");
+    let lines = File.ReadAllLines(path);
+    
+    let solutionA = AOC.one.solveA lines
+    printfn $"solutionA: {solutionA}"
+    
+    let solutionB = AOC.one.solveB lines
+    printfn $"solutionB: {solutionB}"
+
+    
+    
+    
+    0 // return an integer exit code
