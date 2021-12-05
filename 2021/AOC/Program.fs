afb778fded47ce5e92c9b522ef223ed51762a7f5
Michael Radigan <michael@radigan.co.uk>
2021-12-05 20:13:00 +0000

    Somewhat clean up day 2 part a

--- 2021/AOC/Program.fs
+++ 2021/AOC/Program.fs
@@ -6,14 +6,14 @@ open System.Reflection
 
 [<EntryPoint>]
 let main _ =
-    let day = 2    
+    let day = 1  
 
     // This is to get up from the exe directory    
     let projectName = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName
     let path = Path.Combine(projectName, "Inputs", $"{day}.in");
     let lines = File.ReadAllLines(path);
     
-    let solutionA = AOC.Two.solveA lines
+    let solutionA = AOC.one.solveA lines
     printfn $"solutionA: {solutionA}"
     
     // let solutionB = AOC.one.solveB lines
--- 2021/AOC/Two.fs
+++ 2021/AOC/Two.fs
@@ -7,23 +7,19 @@ module AOC.Two
         }
         
     let solveA (lines : string seq) =
-        let pairs = 
-            lines
-            |> Seq.map (fun line ->
-                let splitLine = line.Split(' ')
-                splitLine.[0], splitLine.[1] |> int
-           )
-        
         let folder (state : StateA) ((command, num) : string * int) : StateA =
             match command with
             | "up" -> {Depth = state.Depth - num; Dist = state.Dist}
             | "down" -> {Depth = state.Depth + num; Dist = state.Dist}
             | _ -> {Depth = state.Depth; Dist = state.Dist + num}
-        
-        let finalState =
-            Seq.fold folder {Depth = 0; Dist = 0} pairs
-        
-        finalState.Depth * finalState.Dist    
+            
+        lines
+        |> Seq.map (fun line ->
+            let splitLine = line.Split(' ')
+            splitLine.[0], splitLine.[1] |> int
+        )
+        |> Seq.fold folder {Depth = 0; Dist = 0} 
+        |> (fun finalState -> finalState.Depth * finalState.Dist)  
                            
     let solveB (lines : string seq) =
         failwith "Not implemented"
