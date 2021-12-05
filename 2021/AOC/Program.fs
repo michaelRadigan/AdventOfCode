19782d089e7c3dbdfe821ad4326ff019d0cab77d
Michael Radigan <michael@radigan.co.uk>
2021-12-05 21:21:30 +0000

    Implement day 2 part b

--- 2021/AOC/Program.fs
+++ 2021/AOC/Program.fs
@@ -6,17 +6,17 @@ open System.Reflection
 
 [<EntryPoint>]
 let main _ =
-    let day = 1  
+    let day = 2
 
     // This is to get up from the exe directory    
     let projectName = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName
     let path = Path.Combine(projectName, "Inputs", $"{day}.in");
     let lines = File.ReadAllLines(path);
     
-    let solutionA = AOC.one.solveA lines
+    let solutionA = AOC.Two.solveA lines
     printfn $"solutionA: {solutionA}"
     
-    // let solutionB = AOC.one.solveB lines
-    // printfn $"solutionB: {solutionB}"
+    let solutionB = AOC.Two.solveB lines
+    printfn $"solutionB: {solutionB}"
 
-    0 // return an integer exit code
+    0
--- 2021/AOC/Two.fs
+++ 2021/AOC/Two.fs
@@ -1,10 +1,26 @@
 module AOC.Two
-
+    
     type StateA =
+        {
+            Depth : int;
+            Dist : int;
+        }
+          
+    type StateB =
         {
             Depth : int
             Dist : int
+            Aim : int
         }
+        
+    let solve (lines : string seq) folder empty total =
+       lines
+       |> Seq.map (fun line ->
+            let splitLine = line.Split(' ')
+            splitLine.[0], splitLine.[1] |> int
+       )
+       |> Seq.fold folder empty 
+       |> total
         
     let solveA (lines : string seq) =
         let folder (state : StateA) ((command, num) : string * int) : StateA =
@@ -12,14 +28,14 @@ module AOC.Two
             | "up" -> {Depth = state.Depth - num; Dist = state.Dist}
             | "down" -> {Depth = state.Depth + num; Dist = state.Dist}
             | _ -> {Depth = state.Depth; Dist = state.Dist + num}
-            
-        lines
-        |> Seq.map (fun line ->
-            let splitLine = line.Split(' ')
-            splitLine.[0], splitLine.[1] |> int
-        )
-        |> Seq.fold folder {Depth = 0; Dist = 0} 
-        |> (fun finalState -> finalState.Depth * finalState.Dist)  
+       
+        solve lines folder {Depth = 0; Dist = 0} (fun state -> state.Depth * state.Dist)
                            
     let solveB (lines : string seq) =
-        failwith "Not implemented"
+        let folder (state : StateB) ((command, num) : string * int) : StateB =
+            match command with
+            | "up" -> {Depth = state.Depth; Dist = state.Dist; Aim = state.Aim - num }
+            | "down" -> {Depth = state.Depth; Dist = state.Dist; Aim = state.Aim + num}
+            | _ -> {Depth = state.Depth + num * state.Aim; Dist = state.Dist + num; Aim = state.Aim}
+        
+        solve lines folder {Depth = 0; Dist = 0; Aim = 0} (fun state -> state.Depth * state.Dist)
