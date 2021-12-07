7551723b5f36fc9777b8aa0311e37c10b335d71e
Michael Radigan <michael@radigan.co.uk>
2021-12-07 19:26:33 +0000

    Add mathsy solution to part 2

--- 2021/python/7/solve.py
+++ 2021/python/7/solve.py
@@ -1,6 +1,7 @@
 from aocd.models import Puzzle
 from collections import Counter
-from statistics import median
+from statistics import median, mean
+from math import floor, ceil
 
 
 def triangular(n):
@@ -13,13 +14,22 @@ def solve_a(lines):
 
 
 def solve_b(lines):
-    minimum = min(lines)
-    maximum = max(lines)
-    min_fuel = 999999999
-    for i in range(minimum, maximum+1):
-        fuel = int(sum(map(lambda h: triangular(abs(h - i)), lines)))
-        min_fuel = min(min_fuel, fuel)
-    return min_fuel
+    # The mean point can be between two squares, so check both and grab the min
+    mean_point = mean(lines)
+
+    def cost(square):
+        return int(sum(map(lambda h: triangular(abs(h - square)), lines)))
+    return min(cost(floor(mean_point)), cost(ceil(mean_point)))
+
+
+# def solve_b(lines):
+#     minimum = min(lines)
+#     maximum = max(lines)
+#     min_fuel = 999999999
+#     for i in range(minimum, maximum+1):
+#         fuel = int(sum(map(lambda h: triangular(abs(h - i)), lines)))
+#         min_fuel = min(min_fuel, fuel)
+#     return min_fuel
 
 
 if __name__ == "__main__":
@@ -28,8 +38,8 @@ if __name__ == "__main__":
 
     solution_a = solve_a(lines)
     print(f"Part A: {solution_a}")
-    puzzle.answer_a = solution_a
+    #puzzle.answer_a = solution_a
 
     solution_b = solve_b(lines)
     print(f"Part B: {solution_b}")
-    puzzle.answer_b = solution_b
+    #puzzle.answer_b = solution_b
