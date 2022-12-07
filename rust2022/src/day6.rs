22a90c82683f6cf7821e1f26b2e4cf56016c57f4
Michael Radigan <michael@radigan.co.uk>
2022-12-07 22:16:44 +0000

    Slight tidy up for day 6

--- rust2022/src/day6.rs
+++ rust2022/src/day6.rs
@@ -1,15 +1,13 @@
 use itertools::Itertools;
 
 fn find_marker(input: &str, n : usize) -> usize{
-    let (start_index, window) = 
+    let (start_index, _) = 
         input
         .as_bytes()
         .windows(n)
         .enumerate()
         .find(|(_, window)| window.iter().unique().count() == n)
         .unwrap();
-
-    println!("{:?}", window);
     start_index + n
 }
 
