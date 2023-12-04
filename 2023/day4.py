0c6e8be6d646f60ad60ab76d2e1779838a102ded
Michael Radigan <michael@radigan.co.uk>
2023-12-04 19:42:06 +0000

    Filthy one-liner

--- 2023/day4.py
+++ 2023/day4.py
@@ -1,11 +1,12 @@
 from aocd import get_data
+from functools import reduce
 
 def part_one(data):
-    pass
+    return sum(map(lambda x: 0 if len(x) == 0 else 2 ** (len(x) - 1), [reduce(lambda x , y : x.intersection(y), [set(nums.split()) for nums in line.split(":")[1].strip().split("|")]) for line in data.split("\n")]))
     
 def part_two(data):
     pass
 
 if __name__ == "__main__":
     data = get_data(day=4, year=2023)
-    pass
+    print(f"{part_one(data)=}")
